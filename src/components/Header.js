1bee5775d3bdb41b3692f5649570d47852325043
Nikhil Kumar Singh <nikhilsinghns01@gmail.com>
2022-04-08 13:06:25 +0530

    Projects Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {Container,Nav} from 'react-bootstrap'
+import {Container,Nav,Image} from 'react-bootstrap'
 import { Navbar } from 'react-bootstrap'
 import {NavLink} from './Elements'
+import logo from '../pics/logo.jpeg'
+import {GoLocation} from 'react-icons/go'
+import classes from './style.module.css'
 
 const Header = () => {
     return (
         <React.Fragment>
-           
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">Real Estate</Navbar.Brand>
+                    <Navbar.Brand href="/"> <Image src={logo} height='75' width='75'/></Navbar.Brand>/
+                    <GoLocation color='white'/><p className={classes.city}>Patna</p>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,122 +1,52 @@
 import React from "react"
-import { NavLink } from "react-router-dom";
 import Gallery from '../pics/2.jpg' 
 import building from '../pics/4.jpg'
 import classes from './style.module.css'
 import Galxaxy from '../pics/1.png'
 import concept from '../pics/concept.jpg'
 import three from '../pics/3.jpg'
+import {Carousel,Card} from 'react-bootstrap'
 
 const Projects = () => {
+    
+
     return(
         <React.Fragment>
-            <h1 className={classes.heading}> Upcoming Projects </h1>
-            <br/>
+            <div className={classes.line}/>
+            <Card style={{padding:15,margin:20}}>
+                <Card.Header style={{textAlign : 'center',fontSize:20,fontWeight:'bolder'}}>Bookings Open</Card.Header>
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={building} alt="Building" height={400} width={200}/>
+                    </Carousel.Item>
 
-            <div>
-                <h2 className={classes.projectshead}> The Building </h2>
-                <img src={building} align="right" className={classes.estateImg} alt='building'></img>
-                <p className={classes.process}>
-                    <ul>
-                        <li><b>Delivery : December 2021</b></li>
-                        <li>3BHK , 2BHK</li>
-                        <li>11 Floor Building ,Second Largest of your city.</li>
-                        <li>Undergound Parking</li>
-                        <li>XYZ Road</li>
-                        <li>Conference Hall,Walking area,Swimming Pool,Gym,Playground.</li>
-                        <li>Bookings Open.</li>
-                    </ul>
-                    For More details <NavLink to='/contact' activeClassName={classes.active}>
-                    Contact US
-                        </NavLink>
-                </p>
-            </div>
-            <br/>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={concept} alt="Building" height={500} width={200}/>
+                    </Carousel.Item>
 
-            <div>
-                <h2 className={classes.projectshead}> The Concept </h2>
-                <img src={concept} align="right"className={classes.concept} alt='building'></img>
-                <p className={classes.detailsconcept}>
-                    The Concept is our one of the most luxurious Project which we aim to deliver on June 2023.In this we aim 
-                    to provide every flat as a presedential suite.
-                    This building is in most posh area of your city XYZ.From connectivity point of view its just 30 mins to Airport
-                    and 10 mins to Railway Station. <br/>
-                    Other Facilities : <ul>
-                        <li>24*7 Electricity with Automatic generators for Power Backup</li>
-                        <li>River View.</li>
-                        <li>Gym,Swimming Pool (spearate for Children and Adults), Playground for kids.</li>
-                        <li>Conference Hall</li>
-                        <li>Undergound Parking</li>
-                        <li>Interiors of the Apartment should look like the Photo. </li>
-                    </ul>
-                    For more details.<NavLink to='/contact' activeClassName={classes.active}>
-                    Contact US
-                        </NavLink>
-                </p>
-            </div>
+                </Carousel>
+            </Card>
 
-            <br/>
-            
-            <br/>
-            <h1 className={classes.heading}> Projects Delivered </h1>
-            <div>
-                <h2 className={classes.projectshead}> The Galaxy </h2>
-                <img src={Galxaxy} align="right" className={classes.estateImg} alt='building'></img>
-                <p className={classes.process}>
-                    <ul>
-                        <li><b>Delivered : January 2021</b></li>
-                        <li>3BHK , 2BHK</li>
-                        <li>18 Floor Building,Biggest of your city.</li>
-                        <li>10 floors for Resedential and Rest are for Offices. Top Floor is for residence</li>
-                        <li>Undergound Parking</li>
-                        <li>XYZ Road</li>
-                        <li>Conference Hall,Walking area,Swimming Pool,Gym,Playground.</li>
-                    </ul>
-                    For renting office or residence <NavLink to='/contact' activeClassName={classes.active}>
-                    Contact US
-                        </NavLink>
-                </p>
-            </div>
-            <br/>
-            <div>
-                <h2 className={classes.projectshead}> The Gallery </h2>
-                <img src={Gallery} align="right" className={classes.estateImg} alt='building'></img>
-                <p className={classes.process}>
-                    <ul>
-                        <li><b>Delivered : June 2020</b></li>
-                        <li>3BHK , 2BHK</li>
-                        <li>9 Floor Building </li>
-                        <li>Spacious Parking</li>
-                        <li>500m from XYZ Place</li>
-                        <li>Conference Hall,Walking area,Swimming Pool,Gym,Playground.</li>
-                    </ul>
-                    For renting & leasing <NavLink to='/contact' activeClassName={classes.active}>
-                    Contact US
-                        </NavLink>
-                </p>
-            </div>
+            <div className={classes.line}/>
 
-            <br/>
-            <br/>
-            <br/>
-            <div>
-                <h2 className={classes.projectshead}> The Magellanic Clouds. </h2>
-                <img src={three} align="right" className={classes.concept} alt='building'></img>
-                <p className={classes.process}>
-                    <ul>
-                        <li><b>Delivered : August 2019</b></li>
-                        <li>3BHK,2BHK</li>
-                        <li>10 Floor Building </li>
-                        <li>Spacious Parking</li>
-                        <li>Abc street xyz area.</li>
-                        <li>Conference Hall,Walking area,Swimming Pool,Gym,Playground.</li>
-                    </ul>
-                    For renting & leasing <NavLink to='/contact' activeClassName={classes.active}>
-                    Contact US
-                        </NavLink>
-                </p>
-            </div>
-     
+            <Card style={{padding:15,margin:20}}>
+                <Card.Header style={{textAlign : 'center',fontSize:20,fontWeight:'bolder'}}>Projects we have Delivered</Card.Header>
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={Galxaxy} alt="Building" height={400} width={200}/>
+                        
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={Gallery} alt="Building" height={400} width={200}/>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={three} alt="Building" height={400} width={200}/>
+                    </Carousel.Item>
+                    
+                </Carousel>
+            </Card>
         </React.Fragment>
     )
 }
